e6b31bee1908d648e71f46b99faf86f79ae0f1af
Bruno Martin <bruno.martin@hacklab.com.br>
2015-07-04 18:47:51 -0300

    makes tests python 3.4 compatible

--- djangular/tests/middleware.py
+++ djangular/tests/middleware.py
@@ -11,25 +11,25 @@ class AngularJsonVulnerabilityMiddlewareTest(SimpleTestCase):
         mware = middleware.AngularJsonVulnerabilityMiddleware()
         mware.process_response(request.HttpRequest(), resp)
 
-        self.assertEqual(resp.content, '<html></html>')
+        self.assertEqual(resp.content, b'<html></html>')
 
     def test_that_middleware_does_nothing_to_js_requests(self):
         resp = response.HttpResponse(content_type='text/javascript', content='var blah = [];')
         mware = middleware.AngularJsonVulnerabilityMiddleware()
         mware.process_response(request.HttpRequest(), resp)
 
-        self.assertEqual(resp.content, 'var blah = [];')
+        self.assertEqual(resp.content, b'var blah = [];')
 
     def test_that_middleware_does_nothing_to_invalid_json_requests(self):
         resp = response.HttpResponse(content_type='application/json', content='[1, 2, 3]', status=400)
         mware = middleware.AngularJsonVulnerabilityMiddleware()
         mware.process_response(request.HttpRequest(), resp)
 
-        self.assertEqual(resp.content, '[1, 2, 3]')
+        self.assertEqual(resp.content, b'[1, 2, 3]')
 
     def test_that_middleware_adds_prefix_to_valid_json_requests(self):
         resp = response.HttpResponse(content_type='application/json', content='[1, 2, 3]')
         mware = middleware.AngularJsonVulnerabilityMiddleware()
         mware.process_response(request.HttpRequest(), resp)
 
-        self.assertEqual(resp.content, mware.CONTENT_PREFIX + '[1, 2, 3]')
+        self.assertEqual(resp.content, mware.CONTENT_PREFIX + b'[1, 2, 3]')
